2b76410587fee36b249024fd31c32969742f32c9
Giesen, Jonas <jonas.giesen@hsrw.org>
2026-07-27 23:24:14 +0200

    small changes

--- src/pages/grammatik/components/Building.tsx
+++ src/pages/grammatik/components/Building.tsx
@@ -1,22 +1,35 @@
 import { ExtrudeGeometry, Shape } from "three";
 import type { SegmentType } from "../Grammatik";
 
+let build = false;
+
 export const Building = ({ segment }: { segment: SegmentType }) => {
   const shape = new Shape();
 
-  const diff =
-    segment.left === undefined ? 0.1 : segment.left === true ? 0.2 : -0.2;
+  // const diff =
+  //   segment.left === undefined ? 0.1 : segment.left === true ? 0.2 : -0.2;
 
-  if (segment.start.x !== segment.end.x) {
-    shape.moveTo(segment.start.x, segment.start.z - diff);
-    shape.lineTo(segment.end.x, segment.end.z - diff);
-    shape.lineTo(segment.end.x, segment.end.z + diff);
-    shape.lineTo(segment.start.x, segment.start.z + diff);
+  if (segment.start.x !== segment.end.x && segment.left) {
+    console.log("test");
+
+    const x = segment.start.x - segment.end.x > 0;
+
+    if (x && !build) {
+      console.log("test2");
+      const diff = 0.2;
+      const length = Math.abs(segment.start.x - segment.end.x);
+      shape.moveTo(segment.start.x + diff, segment.start.z + diff + length);
+      shape.lineTo(segment.end.x - diff, segment.start.z + diff + length);
+      shape.lineTo(segment.end.x - diff, segment.start.z + diff);
+      shape.lineTo(segment.start.x + diff, segment.start.z + diff);
+      console.log(shape);
+      build = true;
+    }
   } else {
-    shape.moveTo(segment.start.x - diff, segment.start.z);
-    shape.lineTo(segment.end.x - diff, segment.end.z);
-    shape.lineTo(segment.end.x + diff, segment.end.z);
-    shape.lineTo(segment.start.x + diff, segment.start.z);
+    // shape.moveTo(segment.start.x - diff, segment.start.z);
+    // shape.lineTo(segment.end.x - diff, segment.end.z);
+    // shape.lineTo(segment.end.x + diff, segment.end.z);
+    // shape.lineTo(segment.start.x + diff, segment.start.z);
   }
 
   const geometry = new ExtrudeGeometry(shape, {
